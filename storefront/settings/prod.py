3d1fe8846b33e349ca02142ecdf5e55d356ada11
Mohammadreza Souri <mohammadreza.souri.07@gmail.com>
2024-09-19 20:17:25 +0330

    Config for real product web app

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -7,4 +7,11 @@ DEBUG = False
 SECRET_KEY = os.environ['SECRET_KEY']
 
 
-ALLOWED_HOSTS = []
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+ALLOWED_HOSTS = ['seha1375.pythonanywhere.com']
